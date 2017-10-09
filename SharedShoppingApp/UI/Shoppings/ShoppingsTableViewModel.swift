f360ff492e6394fed8f05e2d1eba5d3f9295ec75
Dariusz Rybicki <darrarski@gmail.com>
2017-10-09 19:45:29 +0200

    Refactor ShoppingsTableViewModel

--- SharedShoppingApp/UI/Shoppings/ShoppingsTableViewModel.swift
+++ SharedShoppingApp/UI/Shoppings/ShoppingsTableViewModel.swift
@@ -2,19 +2,30 @@ import UIKit
 
 class ShoppingsTableViewModel: TableViewControllerInputs {
 
+    init() {
+        let names = ["A", "B", "C", "D"]
+        let shoppings = names.map { Shopping(name: $0) }
+        shoppingRowViewModels = shoppings.map { _ in ShoppingsTableRowViewModel() }
+    }
+
     // MARK: TableViewControllerInputs
 
     func numberOfRows(in section: Int) -> Int {
-        guard section == 0 else { return 0 }
-        return items.count
+        switch section {
+        case 0: return shoppingRowViewModels.count
+        default: fatalError()
+        }
     }
 
     func rowViewModel(at indexPath: IndexPath) -> TableRowViewModel {
-        return ShoppingsTableRowViewModel()
+        switch indexPath.section {
+        case 0: return shoppingRowViewModels[indexPath.row]
+        default: fatalError()
+        }
     }
 
     // MARK: Private
 
-    private let items = ["A", "B", "C", "D"]
+    private let shoppingRowViewModels: [ShoppingsTableRowViewModel]
 
 }
