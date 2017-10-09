c78318e7faa672c3ce929772b25b906023b0f608
Dariusz Rybicki <darrarski@gmail.com>
2017-10-09 22:13:17 +0200

    Add ShoppingsTableViewModelAssembly protocol

--- SharedShoppingApp/UI/Shoppings/ViewModels/Container_ShoppingsTableViewModel.swift
+++ SharedShoppingApp/UI/Shoppings/ViewModels/Container_ShoppingsTableViewModel.swift
@@ -1,7 +1,17 @@
 extension Container {
 
     var shoppingsTableViewModel: ShoppingsTableViewModel {
-        return ShoppingsTableViewModel()
+        return ShoppingsTableViewModel(assembly: Assembly())
+    }
+
+    private struct Assembly: ShoppingsTableViewModelAssembly {
+
+        // MARK: ShoppingsTableViewModelAssembly
+
+        func shoppingsTableRowViewModel(shopping: Shopping) -> ShoppingsTableRowViewModel {
+            return ShoppingsTableRowViewModel(shopping: shopping)
+        }
+
     }
 
 }
--- SharedShoppingApp/UI/Shoppings/ViewModels/ShoppingsTableViewModel.swift
+++ SharedShoppingApp/UI/Shoppings/ViewModels/ShoppingsTableViewModel.swift
@@ -1,31 +1,35 @@
 import UIKit
 
+protocol ShoppingsTableViewModelAssembly {
+    func shoppingsTableRowViewModel(shopping: Shopping) -> ShoppingsTableRowViewModel
+}
+
 class ShoppingsTableViewModel: TableViewControllerInputs {
 
-    init() {
-        let names = ["A", "B", "C", "D"]
-        let shoppings = names.map { Shopping(name: $0, date: Date()) }
-        shoppingRowViewModels = shoppings.map { ShoppingsTableRowViewModel(shopping: $0) }
+    init(assembly: ShoppingsTableViewModelAssembly) {
+        rowViewModels = ["A", "B", "C", "D"]
+            .map { Shopping(name: $0, date: Date()) }
+            .map { assembly.shoppingsTableRowViewModel(shopping: $0) }
     }
 
     // MARK: TableViewControllerInputs
 
     func numberOfRows(in section: Int) -> Int {
         switch section {
-        case 0: return shoppingRowViewModels.count
+        case 0: return rowViewModels.count
         default: fatalError()
         }
     }
 
     func rowViewModel(at indexPath: IndexPath) -> TableRowViewModel {
         switch indexPath.section {
-        case 0: return shoppingRowViewModels[indexPath.row]
+        case 0: return rowViewModels[indexPath.row]
         default: fatalError()
         }
     }
 
     // MARK: Private
 
-    private let shoppingRowViewModels: [ShoppingsTableRowViewModel]
+    private let rowViewModels: [ShoppingsTableRowViewModel]
 
 }
