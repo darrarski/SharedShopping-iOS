659eec2c2e88ffa3687ed2c72a7ade413ba4211d
Dariusz Rybicki <darrarski@gmail.com>
2017-10-18 21:08:36 +0200

    Make Shopping Equatable

--- SharedShoppingApp/Models/Shopping.swift
+++ SharedShoppingApp/Models/Shopping.swift
@@ -4,3 +4,12 @@ struct Shopping {
     let name: String
     let date: Date
 }
+
+extension Shopping: Equatable {
+
+    public static func == (lhs: Shopping, rhs: Shopping) -> Bool {
+        return lhs.name == rhs.name &&
+               lhs.date == rhs.date
+    }
+
+}
