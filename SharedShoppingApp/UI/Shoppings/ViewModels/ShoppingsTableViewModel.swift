343f768c7c0819e28d81af826bee286a96de7646
Dariusz Rybicki <darrarski@gmail.com>
2017-10-19 00:14:48 +0200

    Refactor ShoppingsTableViewModel

--- SharedShoppingApp/UI/Shoppings/ViewModels/ShoppingsTableViewModel.swift
+++ SharedShoppingApp/UI/Shoppings/ViewModels/ShoppingsTableViewModel.swift
@@ -19,33 +19,39 @@ class ShoppingsTableViewModel: TableViewControllerInputs {
 
     func numberOfRows(in section: Int) -> Int {
         switch section {
-        case 0: return rowViewModels.value.count
+        case 0: return rowViewModels.count
         default: fatalError()
         }
     }
 
     func rowViewModel(at indexPath: IndexPath) -> TableRowViewModel {
         switch indexPath.section {
-        case 0: return rowViewModels.value[indexPath.row]
+        case 0: return rowViewModels[indexPath.row]
         default: fatalError()
         }
     }
 
     var event: Observable<TableViewController.Event> {
-        return rowViewModels.asObservable().skip(1).map { _ in TableViewController.Event.reload }
+        return eventSubject.asObservable()
     }
 
     // MARK: Private
 
     private let assembly: ShoppingsTableViewModelAssembly
     private let shoppingsProvider: ShoppingsProviding
-    private let rowViewModels = Variable<[TableRowViewModel]>([])
+    private let eventSubject = PublishSubject<TableViewController.Event>()
     private let disposeBag = DisposeBag()
+
+    private var rowViewModels = [TableRowViewModel]() {
+        didSet {
+            eventSubject.onNext(.reload)
+        }
+    }
 
     private func setupBindings() {
         shoppingsProvider.shoppings
             .map { [weak self] in $0.flatMap { self?.assembly.tableRowViewModel(shopping: $0) } }
-            .bind(to: rowViewModels.asWeakObserver())
+            .subscribe(onNext: { [weak self] in self?.rowViewModels = $0 })
             .disposed(by: disposeBag)
     }
 
