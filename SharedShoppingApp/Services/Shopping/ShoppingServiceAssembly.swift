ade61bd0a4b260c6f8fe1986aa8fcc28c9f529a7
Dariusz Rybicki <darrarski@gmail.com>
2017-10-27 00:21:01 +0200

    Cleanup

--- SharedShoppingApp/Services/Shopping/ShoppingServiceAssembly.swift
+++ SharedShoppingApp/Services/Shopping/ShoppingServiceAssembly.swift
@@ -2,7 +2,7 @@ import Swinject
 
 class ShoppingServiceAssembly: Assembly {
 
-    func assemble(container: Swinject.Container) {
+    func assemble(container: Container) {
         container.register(ShoppingService.self) { _ in
             ShoppingService()
         }.inObjectScope(.container)
