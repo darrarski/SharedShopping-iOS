060684ef4997fafe319f75f178b8fc543cdf875f
Dariusz Rybicki <darrarski@gmail.com>
2017-10-26 23:16:27 +0200

    Add tests to ShoppingsTableRowViewModelSpec

--- SharedShoppingAppTests/UI/Shoppings/ViewModels/ShoppingsTableRowViewModelSpec.swift
+++ SharedShoppingAppTests/UI/Shoppings/ViewModels/ShoppingsTableRowViewModelSpec.swift
@@ -32,6 +32,70 @@ class ShoppingsTableRowViewModelSpec: QuickSpec {
                 )
             }
 
+            describe("other ShoppingsTableRowViewModel with same Shopping") {
+                var other: ShoppingsTableRowViewModel!
+
+                beforeEach {
+                    other = ShoppingsTableRowViewModel(
+                        dateFormatter: DateFormatter(),
+                        rowActionFactory: { UITableViewRowAction(style: $0, title: $1, handler: $2) },
+                        shoppingRemover: ShoppingRemoverSpy(),
+                        shopping: shopping
+                    )
+                }
+
+                it("should be equal") {
+                    expect(sut.isEqual(to: other)).to(beTrue())
+                    expect(other.isEqual(to: sut)).to(beTrue())
+                }
+            }
+
+            describe("other ShoppingsTableRowViewModel with other Shopping") {
+                var other: ShoppingsTableRowViewModel!
+
+                beforeEach {
+                    other = ShoppingsTableRowViewModel(
+                        dateFormatter: dateFormatter,
+                        rowActionFactory: { style, title, handler in
+                            UITableViewRowActionSpy.create(style: style, title: title, handler: handler)
+                        },
+                        shoppingRemover: shoppingRemoverSpy,
+                        shopping: ShoppingFake(name: "Other Shopping", date: Date())
+                    )
+                }
+
+                it("should not be equal") {
+                    expect(sut.isEqual(to: other)).notTo(beTrue())
+                    expect(other.isEqual(to: sut)).notTo(beTrue())
+                }
+            }
+
+            describe("other TableRowViewModel with same Shopping") {
+                var other: TableRowViewModel!
+
+                beforeEach {
+                    other = TableRowViewModelStub(shopping: shopping)
+                }
+
+                it("should not be equal") {
+                    expect(sut.isEqual(to: other)).notTo(beTrue())
+                    expect(other.isEqual(to: sut)).notTo(beTrue())
+                }
+            }
+
+            describe("other TableRowViewModel with other Shopping") {
+                var other: TableRowViewModel!
+
+                beforeEach {
+                    other = TableRowViewModelStub(shopping: ShoppingFake(name: "Other Shopping", date: Date()))
+                }
+
+                it("should not be equal") {
+                    expect(sut.isEqual(to: other)).notTo(beTrue())
+                    expect(other.isEqual(to: sut)).notTo(beTrue())
+                }
+            }
+
             context("register in table view") {
                 var tableView: UITableViewSpy!
 
