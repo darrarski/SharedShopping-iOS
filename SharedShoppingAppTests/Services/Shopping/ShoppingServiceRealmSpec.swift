b963bf6d4db970de94d70157f1b82ac71259833f
Dariusz Rybicki <darrarski@gmail.com>
2017-10-28 18:45:15 +0200

    Add more tests to ShoppingServiceRealmSpec

--- SharedShoppingAppTests/Services/Shopping/ShoppingServiceRealmSpec.swift
+++ SharedShoppingAppTests/Services/Shopping/ShoppingServiceRealmSpec.swift
@@ -16,7 +16,7 @@ class ShoppingServiceRealmSpec: QuickSpec {
             var realm: Realm!
 
             beforeEach {
-                let realmConfig = Realm.Configuration(inMemoryIdentifier: "TestRealm")
+                let realmConfig = Realm.Configuration(inMemoryIdentifier: "ShoppingServiceRealmSpec")
                 realm = try! Realm(configuration: realmConfig)
                 try! realm.write { realm.deleteAll() }
                 sut = ShoppingServiceRealm(realm: realm)
@@ -54,6 +54,74 @@ class ShoppingServiceRealmSpec: QuickSpec {
 
                     it("should have one shopping") {
                         expect(try! sut.shoppings.toBlocking().first()).to(equal([shopping]))
+                    }
+                }
+            }
+
+            describe("with some shoppings") {
+                var shopping1: Shopping!
+                var shopping2: Shopping!
+                var shopping3: Shopping!
+                var shopping4: Shopping!
+                var shopping5: Shopping!
+
+                func createShopping(name: String, date: Date) -> Shopping {
+                    let shopping = ShoppingRealm()
+                    shopping.name = name
+                    shopping.date = date
+                    try! realm.write { realm.add(shopping) }
+                    return shopping
+                }
+
+                beforeEach {
+                    shopping3 = createShopping(name: "Shopping 3", date: Date(timeIntervalSince1970: 30))
+                    shopping5 = createShopping(name: "Shopping 5", date: Date(timeIntervalSince1970: 50))
+                    shopping1 = createShopping(name: "Shopping 1", date: Date(timeIntervalSince1970: 10))
+                    shopping4 = createShopping(name: "Shopping 4", date: Date(timeIntervalSince1970: 40))
+                    shopping2 = createShopping(name: "Shopping 2", date: Date(timeIntervalSince1970: 20))
+                }
+
+                it("should have shoppings ordered by date") {
+                    expect(try! sut.shoppings.toBlocking().first()).to(equal([
+                        shopping1,
+                        shopping2,
+                        shopping3,
+                        shopping4,
+                        shopping5
+                    ]))
+                }
+
+                context("add shopping") {
+                    var newShopping: Shopping!
+
+                    beforeEach {
+                        newShopping = createShopping(name: "New Shopping", date: Date(timeIntervalSince1970: 31))
+                    }
+
+                    it("should have correct shoppings") {
+                        expect(try! sut.shoppings.toBlocking().first()).to(equal([
+                            shopping1,
+                            shopping2,
+                            shopping3,
+                            newShopping,
+                            shopping4,
+                            shopping5
+                        ]))
+                    }
+                }
+
+                context("remove shopping") {
+                    beforeEach {
+                        sut.removeShopping(shopping2)
+                    }
+
+                    it("should have correct shoppings") {
+                        expect(try! sut.shoppings.toBlocking().first()).to(equal([
+                            shopping1,
+                            shopping3,
+                            shopping4,
+                            shopping5
+                        ]))
                     }
                 }
             }
