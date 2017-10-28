8de3b3684fb9c8cbd9bbf4eacc794078dcac6d88
Dariusz Rybicki <darrarski@gmail.com>
2017-10-28 19:20:20 +0200

    Fix isEqual implementation in ShoppingStruct

--- SharedShoppingApp/Models/ShoppingStruct.swift
+++ SharedShoppingApp/Models/ShoppingStruct.swift
@@ -5,6 +5,7 @@ struct ShoppingStruct: Shopping {
     let date: Date
 
     func isEqual(to other: Shopping) -> Bool {
+        guard let other = other as? ShoppingStruct else { return false }
         return name == other.name &&
                date == other.date
     }
