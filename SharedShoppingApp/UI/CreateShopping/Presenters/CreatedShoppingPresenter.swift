91b1994b51d017cf95b6d6f0b0d2edea100a91de
Dariusz Rybicki <darrarski@gmail.com>
2017-11-04 03:14:57 +0100

    Add CreatedShoppingPresenter class and spec

---
+++ SharedShoppingApp/UI/CreateShopping/Presenters/CreatedShoppingPresenter.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class CreatedShoppingPresenter: CreatedShoppingPresenting {
+
+    typealias ViewControllerFactory = (Shopping) -> UIViewController
+
+    init(navigationController: UINavigationController,
+         viewControllerFactory: @escaping ViewControllerFactory) {
+        self.navigationController = navigationController
+        self.viewControllerFactory = viewControllerFactory
+    }
+
+    // MARK: CreatedShoppingPresenting
+
+    func presentCreatedShopping(_ shopping: Shopping) {
+        let viewController = self.viewControllerFactory(shopping)
+        var viewControllers = navigationController.viewControllers
+        _ = viewControllers.popLast()
+        viewControllers.append(viewController)
+        navigationController.setViewControllers(viewControllers, animated: true)
+    }
+
+    // MARK: Private
+
+    private let navigationController: UINavigationController
+    private let viewControllerFactory: ViewControllerFactory
+
+}
